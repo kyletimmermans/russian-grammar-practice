f1a9131648976b7075c53a0841c87d949403dc9d
Kyle Timmermans <timmermansk.17@gmail.com>
2024-08-10 15:22:47 -0400

    Added anchor-hash.js

---
+++ assets/js/anchor-hash.js
@@ -0,0 +1,7 @@
+if (window.location.hash === "#About" || window.location.hash === "#about") {
+	var modal = new bootstrap.Modal(document.getElementById("aboutModal"));
+	modal.show();
+} else if (window.location.hash === "#Exercises" || window.location.hash === "#exercises") {
+	var dropdown = new bootstrap.Dropdown(document.getElementById("dropdownMenu2"));
+	dropdown.show();
+}
