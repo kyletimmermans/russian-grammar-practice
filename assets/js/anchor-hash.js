435854de2cab9afdf5963bf3f5fa068faaa086ed
Kyle Timmermans <timmermansk.17@gmail.com>
2024-08-11 08:26:44 -0400

    Cleanup js files formatting

--- assets/js/anchor-hash.js
+++ assets/js/anchor-hash.js
@@ -1,11 +1,11 @@
 window.addEventListener("load", function() {
-	var modal = new bootstrap.Modal(document.getElementById("aboutModal"));
-	var dropdown = new bootstrap.Dropdown(document.getElementById("dropdownMenu2"));
-	if (window.location.hash.toLowerCase() === "#about") {
-			dropdown.hide();
-			modal.show();
-	} else if (window.location.hash.toLowerCase() === "#exercises") {
-			modal.hide();
-			dropdown.show();
-	}
+    var modal = new bootstrap.Modal(document.getElementById("aboutModal"));
+    var dropdown = new bootstrap.Dropdown(document.getElementById("dropdownMenu2"));
+    if (window.location.hash.toLowerCase() === "#about") {
+        dropdown.hide();
+        modal.show();
+    } else if (window.location.hash.toLowerCase() === "#exercises") {
+        modal.hide();
+        dropdown.show();
+    }
 });
